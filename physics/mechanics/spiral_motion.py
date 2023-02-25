950df7513e96fa4e8005184f6a6c047b90349e27
Muntazir Abidi <30961590+muntazirabidi@users.noreply.github.com>
2023-02-25 17:45:59 +0500

    Update spiral_motion.py

--- physics/mechanics/spiral_motion.py
+++ physics/mechanics/spiral_motion.py
@@ -3,17 +3,29 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.patches import Rectangle
 from matplotlib.widgets import Button
+from PIL import Image
 
 # Set up the figure and axis
-fig = plt.figure(figsize=(10, 10))
+fig = plt.figure(figsize=(12, 12))
 ax = plt.axes(xlim=(-1.5, 1.5), ylim=(-1.5, 1.5))
 ax.set_aspect('equal')
+fig.patch.set_facecolor('lightblue')
+
+# Add a title and axis labels
+plt.title('Particle Spiral Animation')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Load a background image and display it behind the plot
+bg_image = Image.open('background.jpg')
+ax.imshow(bg_image, extent=(-1.5, 1.5, -1.5, 1.5), aspect='auto')
 
 # Initialize the particle
 particle, = ax.plot([], [], 'o', ms=15, mfc='brown', mec='white', mew=2, zorder=10)
 
+
 # Initialize the spiral curve
-curve, = ax.plot([], [], '--', color='gray', lw=2)
+curve, = ax.plot([], [], '--', color='magenta', lw=3)
 
 # Add x and y axes
 ax.axhline(0, color='gray', lw=1.5, alpha=0.5)
@@ -23,6 +35,10 @@ ax.axvline(0, color='gray', lw=1.5, alpha=0.5)
 vector, = ax.plot([], [], 'r', lw=1.5, label=r'Position vector $\vec{r}$', zorder=5)
 r_unit_arrow = None
 theta_unit_arrow = None
+r_unit_text = None
+theta_unit_text = None
+
+
 scale_factor = 0.3
 
 # Add legend to the plot
@@ -30,23 +46,31 @@ ax.legend(loc='upper left', fontsize=12)
 
 # Initialize the angle text annotation
 angle_text = ax.text(1.3, 1.3, '', fontsize=16, ha='center', va='center')
+
 # Initialize the position text annotation
 position_text = ax.text(1.1, 1.1, '', fontsize=16, ha='center', va='center')
+
 # Initialize the radius text annotation
 radius_text = ax.text(-1.8, 1.7, '', fontsize=16, ha='left', va='center')
 
-
 # Define the function to update the particle and position vector
 def update_particle(frame):
-    global r_unit_arrow, theta_unit_arrow
+    global r_unit_arrow, theta_unit_arrow, r_unit_text, theta_unit_text
     
-    # Clear the previous unit vectors
+    # Clear the previous unit vectors and text annotations
     if r_unit_arrow:
         r_unit_arrow.remove()
         r_unit_arrow = None
     if theta_unit_arrow:
         theta_unit_arrow.remove()
         theta_unit_arrow = None
+    if r_unit_text:
+        r_unit_text.remove()
+        r_unit_text = None
+    if theta_unit_text:
+        theta_unit_text.remove()
+        theta_unit_text = None
+    
     
     # Compute the new position and radius
     t = 5 * frame  # time-dependent term for radius
@@ -57,25 +81,40 @@ def update_particle(frame):
     
     # Compute the position vector
     vector.set_data([0, x], [0, y])
-    
+
     # Compute the unit vectors of r and theta
     r = np.array([x, y])
     r_unit_vec = r / np.linalg.norm(r)
     v = np.array([-y, x])
     theta_unit_vec = v / np.linalg.norm(v)
-    
+
     # Update the unit vectors with scaling
-    r_unit_arrow = ax.arrow(x, y, scale_factor * r_unit_vec[0], scale_factor * r_unit_vec[1], head_width=0.05, head_length=0.1, fc='g', ec='g', lw=3)
+    r_unit_arrow = ax.arrow(x, y, scale_factor * r_unit_vec[0], scale_factor * r_unit_vec[1], head_width=0.05, head_length=0.1, fc='k', ec='k', lw=3)
     theta_unit_arrow = ax.arrow(x, y, scale_factor * theta_unit_vec[0], scale_factor * theta_unit_vec[1], head_width=0.05, head_length=0.1, fc='b', ec='b', lw=3)
+
+    # Add text annotations for the unit vectors
+    r_unit_text = ax.text(x + scale_factor * r_unit_vec[0], y + scale_factor * r_unit_vec[1], r"$\hat{r}$", fontsize=16,color='white')
+    theta_unit_text = ax.text(x + scale_factor * theta_unit_vec[0], y + scale_factor * theta_unit_vec[1], r"$\hat{\theta}$", fontsize=16, color='white')
+    
 
     # Update the angle text annotation
     angle_degrees = np.degrees(5*frame)
     angle_text.set_text(r'$\theta$: {:.1f}°'.format(angle_degrees % 360))
     angle_text.set_position((0.88, 1.3))
+    angle_text.set_fontsize(16)
+    angle_text.set_fontfamily('serif')
+    angle_text.set_fontweight('bold')
+    angle_text.set_color('white')
+
 
     # Update the position text annotation
     position_text.set_text(r'$r: $({:.2f}, {:.2f})'.format(x, y))
     position_text.set_position((1.0, 1.05))
+    position_text.set_fontsize(16)
+    position_text.set_fontfamily('sans-serif')
+    position_text.set_fontstyle('normal')
+    position_text.set_fontweight('bold')
+    position_text.set_color('white')
 
     # Compute the spiral curve
     t_values = np.linspace(0, 5 * frame, 1000)
@@ -147,12 +186,11 @@ stop_button.label.set_fontsize(14)
 stop_button.label.set_fontweight('bold')
 resume_button.label.set_fontsize(14)
 resume_button.label.set_fontweight('bold')
-#reset_button.label.set_fontsize(14)
-#reset_button.label.set_fontweight('bold')
+
 
 # Show the animation
 plt.show()
 
 # Save the animation as a GIF using Pillow
-writer = animation.PillowWriter(fps=5)
-ani.save("particle_spiral.gif", writer=writer)
+#writer = animation.PillowWriter(fps=5)
+#ani.save("particle_spiral.gif", writer=writer)
